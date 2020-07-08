1712b4a9369bac74c01a0b62fc746833ef12a11f
AK Hanish <akhanish7@gmail.com>
2020-07-09 00:50:03 +0530

    PassportJs Implemented

---
+++ config/passport_setup.js
@@ -0,0 +1,66 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
+const conn = require('./db');
+
+module.exports = (passport) => {
+  passport.serializeUser((profile, done) => {
+    done(null, profile);
+  });
+
+  passport.deserializeUser((id, done) => {
+    conn.query(
+      'SELECT * FROM user WHERE google_id=?',
+      id,
+      (err, response, meta) => {
+        console.log(id);
+        done(null, response[0]);
+      }
+    );
+  });
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect',
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log('passport callback function fired:');
+
+        var name = profile._json.name;
+        var id = profile.id;
+        var picture = profile._json.picture;
+
+        conn.query(
+          'select * from user where google_id=?',
+          id,
+          (err, response, meta) => {
+            if (response[0]) {
+              console.log('aready exists');
+              done(null, response[0]);
+            } else if (err) {
+              console.log(err);
+            } else {
+              conn.query(
+                'Insert into user (name,google_id,picture)  values(?,?,?)',
+                [name, id, picture],
+                (err, result) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    // console.log(result);
+                    done(null, { name: name, google_id: id, picture: picture });
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    )
+  );
+};
