653084f7df4ab2e039340bc446e04522e1755d7e
AK Hanish <akhanish7@gmail.com>
2020-07-17 04:18:30 +0530

    DB Working

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 module.exports = {
   google: {
-    clientID:
-      '773547824564-0f1k9sdhsd7rmarckm9hsavk1q3a6853.apps.googleusercontent.com',
-    clientSecret: 'qtpZku-AFZNjzbn_w1MiXXLX',
+    clientID: '',
+    clientSecret: '',
   },
   facebook: {
     appID: '',
